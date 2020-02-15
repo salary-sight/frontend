19e6121b1f92dc98b507c7c77121f12d20b93fe8
H-Richard <r.hong@pm.me>
2020-02-15 10:10:35 -0500

    Added loaders

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.scss';
 import Autocomplete from './components/Autocomplete/Autocomplete';
 import Button from './components/Button/Button';
 import Particles from 'react-particles-js';
+import { RotateSpinner } from "react-spinners-kit";
 import {
   createMuiTheme,
   ThemeProvider
 } from "@material-ui/core";
 import FadeIn from 'react-fade-in';
-const options = ["React", "ReactNative", "R", "Rs", "Ra", "Rf", "asdklfjnkjladsfhk jhadkjsfhkjasdf", "asdfasdfasdfasdfasdfasdfasd"]
+
+import { options, loadingText } from './vars';
 
 const theme = createMuiTheme({
   palette: {
@@ -35,37 +37,51 @@ const theme = createMuiTheme({
 
 type AppState = {
   skills: string[],
-  step: number
+  step: number,
+  fadeOut: boolean,
+  loadingText: string[],
+  loadingTextIndex: number,
 }
+
+
 
 export default class App extends React.Component<any, AppState>{
   constructor(props:any){
     super(props);
     this.state={
       skills: [],
-      step: 0
+      step: 0,
+      fadeOut: false,
+      loadingText: loadingText,
+      loadingTextIndex: 0
     }
   }
 
-  checkForm = () =>{
-    if(this.state.skills){
-      this.setState({
-        step: this.state.step + 1,
-      })
-    }
+  startAlternate = () => {
+    setTimeout(() => this.setState({
+      loadingTextIndex: this.state.loadingTextIndex + 1
+    }), 3200)
+  }
+
+  checkForm = () => {
+    this.setState({
+      fadeOut: true
+    })
+    setTimeout(() => this.setState({
+      step: this.state.step + 1
+    }), 800)
   }
 
   render(){
     console.log(this.state.skills);
-    const { step } = this.state;
+    const { step, loadingText, loadingTextIndex } = this.state;
     switch(step){
       case 0:
         return(
           <ThemeProvider theme={theme}>
           <div className="App">
-          
           <Particles />
-            <div className="FormContainer">
+            <div className={this.state.fadeOut ? "FormContainer FadeOut" : "FormContainer"}>
               <div className="TextContaine">
                 <h1>S
                   <span style={{color: '#fe6b8a', border: "2px solid white", fontSize: 36}}>
@@ -74,11 +90,11 @@ export default class App extends React.Component<any, AppState>{
                 <h3>Discover your salary around the world</h3>
               </div>
               <FadeIn transitionDuration={600}>
-              <div className="Form Box">
+              <div className={"Form Box"}>
               <FadeIn transitionDuration={600} delay={150}>
                 <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
                 <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
-                <Button text={"Submit"}/>
+                <Button text={"Submit"} onClick={this.checkForm}/>
               </FadeIn>   
               </div>
               </FadeIn>
@@ -88,22 +104,18 @@ export default class App extends React.Component<any, AppState>{
         </ThemeProvider>
         );
       case 1:
+        this.startAlternate();
         return(
           <ThemeProvider theme={theme}>
             <div className="App">
+            <Particles />
               <div className="FormContainer">
-                <div className="TextContainer">
-                  <h1>S
-                    <span style={{color: '#fe6b8a', border: "2px solid white", fontSize: 36}}>
-                      al</span>ary <span style={{color: '#fe8167'}}>Sight</span>
-                  </h1>
-                  <h3>Discover your salary around the world</h3>
-                </div>
-                <div className="Form">
-                  <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
-                  <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
-                  <Button text={"Submit"}/>
-                </div>
+                <FadeIn>
+                  <div className="Form Box">
+                    <RotateSpinner color="#ff8e53"/>
+                    <h1 style={{marginTop: -38}}>{loadingText[loadingTextIndex % loadingText.length]}</h1>
+                  </div>
+                </FadeIn>
               </div>
             </div>
           </ThemeProvider>
---
+++ src/vars.tsx
@@ -0,0 +1,7 @@
+export const options = ["React", "ReactNative", "R", "Rs", "Ra", "Rf", "asdklfjnkjladsfhk jhadkjsfhkjasdf", "asdfasdfasdfasdfasdfasdfasd"]
+export const loadingText = ["Hello World",
+                            "Are we getting paid for this?",
+                            "Slapping Watermelons",
+                            "Someone please fund my startup",
+                            "When do we get food?",
+                            "Monkeys wrote this code"]
