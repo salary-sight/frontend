50425dea1e0269e5073176a1f48689d473324def
H-Richard <r.hong@pm.me>
2020-02-15 06:15:49 -0500

    Added switch case in render

--- src/App.tsx
+++ src/App.tsx
@@ -33,34 +33,67 @@ const theme = createMuiTheme({
   }
 });
 
-export default class App extends React.Component{
+type AppState = {
+  skills: string[],
+  step: number
+}
+
+export default class App extends React.Component<any, AppState>{
   constructor(props:any){
     super(props);
     this.state={
-      skills: []
+      skills: [],
+      step: 0
     }
   }
 
   render(){
-    return (
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <div className="FormContainer">
-            <div className="TextContainer">
-              <h1>S
-                <span style={{color: '#fe6b8a', border: "2px solid white", fontSize: 36}}>
-                  al</span>ary <span style={{color: '#fe8167'}}>Sight</span>
-              </h1>
-              <h3>Discover your salary around the world</h3>
-            </div>
-            <div className="Form">
-              <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
-              <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
-              <Button text={"Submit"}/>
+    const { step } = this.state;
+    switch(step){
+      case 0:
+        return(
+          <ThemeProvider theme={theme}>
+          <div className="App">
+            <div className="FormContainer">
+              <div className="TextContaine">
+                <h1>S
+                  <span style={{color: '#fe6b8a', border: "2px solid white", fontSize: 36}}>
+                    al</span>ary <span style={{color: '#fe8167'}}>Sight</span>
+                </h1>
+                <h3>Discover your salary around the world</h3>
+              </div>
+              <div className="Form Box">
+                <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
+                <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
+                <Button text={"Submit"}/>
+              </div>
             </div>
           </div>
-        </div>
-      </ThemeProvider>
-    );
+        </ThemeProvider>
+        );
+      case 1:
+        return(
+          <ThemeProvider theme={theme}>
+            <div className="App">
+              <div className="FormContainer">
+                <div className="TextContainer">
+                  <h1>S
+                    <span style={{color: '#fe6b8a', border: "2px solid white", fontSize: 36}}>
+                      al</span>ary <span style={{color: '#fe8167'}}>Sight</span>
+                  </h1>
+                  <h3>Discover your salary around the world</h3>
+                </div>
+                <div className="Form">
+                  <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
+                  <Autocomplete options={options} id="skills" setValue={(arr: string[]) => this.setState({ skills: arr })} label="skills" placeholder="javascript"/>
+                  <Button text={"Submit"}/>
+                </div>
+              </div>
+            </div>
+          </ThemeProvider>
+        );
+    }
   }
 }
+
+
